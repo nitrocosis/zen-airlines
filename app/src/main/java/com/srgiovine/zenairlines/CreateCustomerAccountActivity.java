65b4d68f73669cc2167bab3030f1e14f3f1383ad
Sarah Giovine <nitrocosis@gmail.com>
2015-12-07 23:22:50 -0500

    Completedlayout for CreateCustomerAccountActivity.

--- app/src/main/java/com/srgiovine/zenairlines/CreateCustomerAccountActivity.java
+++ app/src/main/java/com/srgiovine/zenairlines/CreateCustomerAccountActivity.java
@@ -1,7 +1,10 @@
 package com.srgiovine.zenairlines;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Allows passengers to create a customer account.
@@ -13,4 +16,13 @@ public class CreateCustomerAccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_customer_account);
     }
+
+    public void onCreateCustomerAccountButtonClicked(View view) {
+        // TODO Replace with real implementation
+        new AlertDialog.Builder(this)
+                .setMessage("Your customer id is " + 1)
+                .setPositiveButton("Got it!", null)
+                .show();
+    }
+
 }
